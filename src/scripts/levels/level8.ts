2695dbc36a9ee3f05f33e878c4503079ee43533a
Madu <kontakt.madu@gmail.com>
2024-01-25 19:30:00 +0100

    Goodbye character

--- src/scripts/levels/level8.ts
+++ src/scripts/levels/level8.ts
@@ -3,14 +3,16 @@ import { NetzlerLevel } from '../classes/NetzlerLevel';
 import { confetti, drawCertificate, switchTool } from '../NetzlerFunctions';
 
 export const level8: NetzlerLevel = new NetzlerLevel([{
-  message: 'ICH BIN ERSTAUNT *Quak*!',
+  message: 'ICH BIN ERSTAUNT *Quak*! Ich habe für dich ein Zertifikat erstellt! Lade es dir doch runter, indem du mit deiner linken Maustaste drauf klickst! Wenn du noch nicht genug hast, klick doch mal auf die Sprechblase, um das Konfetti nochmal schießen zu lassen. Du kannst mich aber auch dazu bringen, einen Salto zu machen. Nun gut, ich hoffe ich konnte dir im Thema Netzwerk-Konfiguration helfen! Hab noch einen schönen Tag *Quak* *Quak*!',
   action: (): void => { 
     confetti();
     drawCertificate();
     switchTool('download');
     Globals.finished = true;
     const messagebox: HTMLElement = document.getElementById('character-message-box');
+    const characterImage: HTMLElement = document.getElementById('character-image');
     messagebox.classList.add('party-cursor');
+    characterImage.classList.add('pointer-cursor');
     Globals.canvasElement.addEventListener('click', () => {
       const aTag: HTMLAnchorElement = <HTMLAnchorElement>document.getElementById('zertifikat-download-a');
       let url: string = Globals.canvasElement.toDataURL('image/png');
@@ -21,6 +23,12 @@ export const level8: NetzlerLevel = new NetzlerLevel([{
     });
     messagebox.addEventListener('click', () => {
       confetti();
+    });
+    characterImage.addEventListener('click', () => {
+      characterImage.classList.add('rotate');
+      setTimeout(() => {
+        characterImage.classList.remove('rotate');
+      }, 1000);
     });
   }
 }], [], []);
